a448675e119ba83f09553f6262009219cd69442f
johnsonafool <88415078+johnsonafool@users.noreply.github.com>
2022-09-25 13:10:40 +0800

    create project basic setting and eslint

---
+++ run.sh
@@ -0,0 +1,11 @@
+# init
+yarn create expo-app rate-repository-app --template expo-template-blank@sdk-46                       
+
+# install packages
+yarn expo install react-native-web@~0.18.7 react-dom@18.2.0 @expo/webpack-config@^0.17.0                
+
+# launch ios simulator
+yarn expo start
+
+# install eslint
+yarn add --save-dev eslint @babel/eslint-parser eslint-plugin-react eslint-plugin-react-native
